81ead2dd6319d197af9b6c8b58f44dfb07888ae4
Grant <grant1993@live.com>
2023-12-06 13:40:51 +0000

    removed the rels

--- src/Socials.js
+++ src/Socials.js
@@ -3,14 +3,23 @@ import linkedIn from "./linkedin.png";
 import github from "./github.png";
 
 function Socials() {
-
-
   return (
-  <div class="wrapper">
-    <div class="one"><a href="https://www.linkedin.com/in/grant-pritchard-00401b199" rel="noopener" target="_blank"><img src={linkedIn} alt="LinkedIn" /></a></div>
-    <div class="two"><a href="https://github.com/Grant1993" rel="noopener" target="_blank"><img src={github} alt="Github" /></a></div>
-  </div>
-  )
+    <div class="wrapper">
+      <div class="one">
+        <a
+          href="https://www.linkedin.com/in/grant-pritchard-00401b199"
+          target="_blank"
+        >
+          <img src={linkedIn} alt="LinkedIn" />
+        </a>
+      </div>
+      <div class="two">
+        <a href="https://github.com/Grant1993" target="_blank">
+          <img src={github} alt="Github" />
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Socials;
