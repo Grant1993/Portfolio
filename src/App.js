5b6bbd648ff2008f21cf11192811e7b6d23201cb
Grant <grant1993@live.com>
2023-12-07 17:08:43 +0000

    added a scroll to the top button

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Projects from "./Projects";
 import Skills from "./Skills";
 import Certifications from "./Certifications";
 import "./App.css";
+import ScrollToTheTop from "./ScrollToTheTop";
 
 function App() {
   return (
     <div className="App">
+      <ScrollToTheTop />
       <Links />
       <Brief />
       <Projects />
---
+++ src/ScrollToTheTop.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import up from "./up.png";
+
+function ScrollToTheTop() {
+  const [scrollToTheTop, setScrollToTheTop] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        setScrollToTheTop(true);
+      } else {
+        setScrollToTheTop(false);
+      }
+    });
+  }, []);
+
+  const scrollUp = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+  return (
+    <div>
+      {scrollToTheTop && (
+        <button
+          style={{
+            position: "fixed",
+            bottom: "50px",
+            right: "50px",
+            height: "50px",
+            width: "50px",
+            fontSize: "35px",
+          }}
+          onClick={scrollUp}
+        >
+          ⮝
+        </button>
+      )}
+    </div>
+  );
+}
+
+export default ScrollToTheTop;
